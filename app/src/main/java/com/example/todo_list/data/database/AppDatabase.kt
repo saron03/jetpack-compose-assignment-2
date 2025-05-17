61b9b1efa71a0306b93bd9c84c93f9f1a1967e22
saron03 <saronyitbe@gmail.com>
2025-05-17 13:55:08 +0300

    Implement data layer: Local database setup and management

--- app/src/main/java/com/example/todo_list/data/database/AppDatabase.kt
+++ app/src/main/java/com/example/todo_list/data/database/AppDatabase.kt
@@ -1,4 +1,29 @@
 package com.example.todo_list.data.database
 
-class AppDatabase {
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.example.todo_list.data.model.ToDo
+
+@Database(entities = [ToDo::class], version = 1, exportSchema = false)
+abstract class AppDatabase : RoomDatabase() {
+    abstract fun todoDao(): ToDoDao
+
+    companion object {
+        @Volatile
+        private var INSTANCE: AppDatabase? = null
+
+        fun getDatabase(context: Context): AppDatabase {
+            return INSTANCE ?: synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    AppDatabase::class.java,
+                    "todo_database"
+                ).build()
+                INSTANCE = instance
+                instance
+            }
+        }
+    }
 }
--- app/src/main/java/com/example/todo_list/data/database/TodoDao.kt
+++ app/src/main/java/com/example/todo_list/data/database/TodoDao.kt
@@ -1,4 +1,22 @@
 package com.example.todo_list.data.database
 
-interface TodoDao {
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.example.todo_list.data.model.ToDo
+
+@Dao
+interface ToDoDao {
+    @Query("SELECT * FROM todos")
+    suspend fun getAllTodos(): List<ToDo>
+
+    @Query("SELECT * FROM todos WHERE id = :id")
+    suspend fun getTodoById(id: Int): ToDo?
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertTodos(todos: List<ToDo>)
+
+    @Query("DELETE FROM todos")
+    suspend fun clearTodos()
 }
